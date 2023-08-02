772c2f7744a3b28a2866f6b65b5d6489d842b6e3
thanhchi2kbn <thanhchi2kbn@gmail.com>
2023-08-02 12:00:51 +0700

    update modal confirm for remove todo

---
+++ src/Component/ModalConfirm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ModalConfirm = ({ show, onClose, onConfirm, message }) => {
+  if (!show) return null;
+
+  return (
+    <div className="custom-modal">
+      <div className="modal-content">
+        <h3>Confirm</h3>
+        <p>{message}</p>
+        <div className="d-flex justify-content-end">
+          <button onClick={onConfirm} className='btn btn-danger me-1'>
+            Yes
+          </button>
+          <button onClick={onClose} className='btn btn-secondary'>
+            No
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalConfirm;
--- src/Component/TodoItem.js
+++ src/Component/TodoItem.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
+import ModalConfirm from './ModalConfirm';
 
-export default function TodoItem({ todo, index, deleteTodo, toggleCompleted,handleEditTodo }) {
+export default function TodoItem({ todo, index, deleteTodo, toggleCompleted, handleEditTodo }) {
   const [editing, setEditing] = useState(false);
   const [editedText, setEditedText] = useState(todo.text);
-
-  const handleDelete = () => {
-    deleteTodo(index);
-  };
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const handleToggle = () => {
     toggleCompleted(index);
@@ -21,7 +19,7 @@ export default function TodoItem({ todo, index, deleteTodo, toggleCompleted,hand
       ...todo,
       text: editedText,
     };
-    handleEditTodo(index, editedTodo); 
+    handleEditTodo(index, editedTodo);
     setEditing(false);
   };
 
@@ -30,26 +28,48 @@ export default function TodoItem({ todo, index, deleteTodo, toggleCompleted,hand
     setEditedText(todo.text);
   };
 
+  const handleOpenModal = () => {
+    setShowDeleteModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowDeleteModal(false);
+  };
+
+  const handleDeleteConfirmed = () => {
+    setShowDeleteModal(false);
+    deleteTodo(index);
+  };
+
   return (
-    <li className={`d-flex justify-content-between align-items-center ${todo.completed ? 'completed' : ''}`}>
-      {editing ? (
-        <>
-          <input type="text" value={editedText} onChange={(e) => setEditedText(e.target.value)} />
-          <div>
-            <button onClick={handleSave} className='btn btn-success'><i className="fa-solid fa-check"></i></button>
-            <button onClick={handleCancel} className='btn btn-secondary ms-1'><i className="fa-solid fa-times"></i></button>
-          </div>
-        </>
-      ) : (
-        <>
-          <p>{todo.text}</p>
-          <div>
-            <button onClick={handleDelete} className='btn btn-danger me-1'><i className="fa-solid fa-trash"></i></button>
-            <button onClick={handleToggle} className='btn btn-success'><i className={todo.completed ? "fa-solid fa-rotate-left" : "fa-solid fa-check"}></i></button>
-            <button onClick={handleEdit} className='btn btn-primary ms-1'><i className="fa-solid fa-pencil"></i></button>
-          </div>
-        </>
-      )}
-    </li>
+    <div>
+      <li className={`d-flex justify-content-between align-items-center ${todo.completed ? 'completed' : ''}`}>
+        {editing ? (
+          <>
+            <input type="text" value={editedText} onChange={(e) => setEditedText(e.target.value)} />
+            <div>
+              <button onClick={handleSave} className='btn btn-success'><i className="fa-solid fa-check"></i></button>
+              <button onClick={handleCancel} className='btn btn-secondary ms-1'><i className="fa-solid fa-times"></i></button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p>{todo.text}</p>
+            <div>
+              <button onClick={handleOpenModal} className='btn btn-danger me-1'><i className="fa-solid fa-trash"></i></button>
+              <button onClick={handleToggle} className='btn btn-success'><i className={todo.completed ? "fa-solid fa-rotate-left" : "fa-solid fa-check"}></i></button>
+              <button onClick={handleEdit} className='btn btn-primary ms-1'><i className="fa-solid fa-pencil"></i></button>
+            </div>
+          </>
+        )}
+      </li>
+      {/* Delete confirmation modal */}
+      <ModalConfirm
+        show={showDeleteModal}
+        onClose={handleCloseModal}
+        onConfirm={handleDeleteConfirmed}
+        message="Are you sure you want to delete this todo?"
+      />
+    </div>
   );
 }
